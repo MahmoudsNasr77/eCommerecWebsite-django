b9845fe11fa447b97c50a7769939236b4d2b120f
Mahmoud Nasr <67973033+MahmoudsNasr77@users.noreply.github.com>
2022-10-14 19:29:01 +0200

    Update settings.py

--- E_commenrce/settings.py
+++ E_commenrce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z7gsq6y942%x@j8lc$+3pkqvcx5*4!49an7nyb5jjm*%e9sm5y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
