848e3e89b4a030cc6ca07f3412aaba3cd7dce80b
Mahmoud Nasr <67973033+MahmoudsNasr77@users.noreply.github.com>
2022-10-16 19:49:27 +0200

    Update urls.py

--- E_commenrce/urls.py
+++ E_commenrce/urls.py
@@ -19,7 +19,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls',namespace='products')),
-    path('accounts/', include('accounts.urls',namespace='accounts_detials')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    
 ]
